54b1c0e72074bef1db13eb7801b3eee578359537
Mateus <mateuscamargo2908@gmail.com>
2024-04-05 19:32:34 -0300

    docs game.js

--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -1,14 +1,24 @@
+//<div class="cell"></div>
 const cell          = document.querySelectorAll('.cell');
+
+//<h2 id="game-info">player <span id="player-info">1</span> turn</h2>
 const gameInfo      = document.getElementById('game-info');
 const playerInfo    = document.getElementById('player-info');
 
+//<h3><span class="player-1"   id="points-info-1">0</span></h3>
+//<h3><span class="player-2"   id="points-info-2">0</span></h3>
+//<h3><span class="player-tie" id="points-info-tie">0</span></h3>
 const pointsInfo1   = document.getElementById('points-info-1');
 const pointsInfo2   = document.getElementById('points-info-2');
 const pointsInfoTie = document.getElementById('points-info-tie');
 
+//<button id="reset">reset</button>
 const reset         = document.getElementById('reset');
 
+//set player 1 as the first player
 playerInfo.classList.add('player-1');
+
+//define win combinations
 const combinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -20,46 +30,62 @@ const combinations = [
     [2, 4, 6]
 ]
 
+//player-specific arrays
 let player1 = [];
 let player2 = [];
 
+//players and ties points
 let player1Points = 0;
 let player2Points = 0;
 let tiePoints     = 0;
 
+//boolean variables
 let canPlay  = true;
 let flag     = true;
-let numPlays = 0;
+
+//number of moves
+let numMoves = 0;
 
 
 //run throug the 'cell' array
 for (let i = 0; i < cell.length; i ++) {
     
+    //event listener for cell clicks
     cell[i].addEventListener('click', () => {
-        numPlays ++;
+        numMoves ++;
 
-        //check if players can play the game
+        //check if players can make a move
         if (canPlay === true) {
             
             //check wich one is playing
             if (flag === true) {
-                    cell[i].classList.remove('player-2');
-                    cell[i].classList.add('player-1');
-                    cell[i].innerHTML = 'x';
+                //make the move
+                cell[i].classList.remove('player-2');
+                cell[i].classList.add('player-1');
+                cell[i].innerHTML = 'x';
 
-                    gameInfo.innerHTML = "player <span class='player-2'>2</span> turn"
+                //update game info
+                gameInfo.innerHTML = "player <span class='player-2'>2</span> turn"
 
-                    player1.push(i);
-                    flag = false;
+                //add move to player array
+                player1.push(i);
+
+                //change player
+                flag = false;
             } else {
-                    cell[i].classList.remove('player-1');
-                    cell[i].classList.add('player-2');
-                    cell[i].innerHTML = 'o';
+                //make the move
+                cell[i].classList.remove('player-1');
+                cell[i].classList.add('player-2');
+                cell[i].innerHTML = 'o';
 
-                    gameInfo.innerHTML = "player <span class='player-1'>1</span> turn"
+                //update game info
+                gameInfo.innerHTML = "player <span class='player-1'>1</span> turn"
+            
+                //add move to player array
+                player2.push(i);
 
-                    player2.push(i);
-                    flag = true;
+                //change player
+                flag = true;
             }
 
             checkWinner();
@@ -69,6 +95,8 @@ for (let i = 0; i < cell.length; i ++) {
 
 //check for winner
 function checkWinner() {
+
+    //iterate each element in the 'combinations' array
     for (const combination of combinations) {
         const [a, b, c] = combination;
 
@@ -76,14 +104,15 @@ function checkWinner() {
         if (player1.includes(a) &&
             player1.includes(b) &&
             player1.includes(c)) {
+            //add point
             player1Points ++;
             pointsInfo1.innerText = player1Points;
+
+            //end game
             canPlay = false;
-            //add more logic (display message, add classes on css, end the game...)
             
             //display winner
             gameInfo.innerHTML = "player <span id='player-info' class='player-1'>1</span> wins!";
-
             return;
         }
 
@@ -91,21 +120,27 @@ function checkWinner() {
         if (player2.includes(a) &&
             player2.includes(b) &&
             player2.includes(c)) {
+            //add point
             player2Points ++;
             pointsInfo2.innerText = player2Points;
+
+            //end game
             canPlay = false;
-            //add more logic (display message, add classes on css, end the game...)
 
             //display winner
             gameInfo.innerHTML = "player <span class='player-2'>2</span> wins!";
             return;
         }
 
-        if (numPlays >= 9) {
+        //check for a tie
+        if (numMoves >= 9) {
+            //add point
             tiePoints ++;
             pointsInfoTie.innerText = tiePoints;
-            canPlay = false;
             
+            //end game
+            canPlay = false;
+
             //display tie
             gameInfo.innerHTML = "<span class='tie'>tie</span>!";
             return;
@@ -122,7 +157,7 @@ reset.addEventListener('click', () => {
     player2  = [];
     canPlay  = true;
     flag     = true;
-    numPlays = 0;
+    numMoves = 0;
     
     //reset innerHTML
     gameInfo.innerHTML = "player <span class='player-1'>1</span> turn";
